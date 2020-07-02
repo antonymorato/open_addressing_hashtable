3e0fd5fe993e1cbe4013f7f866400393e81c68dd
antonymorato <antonymorato2@gmail.com>
2020-07-02 05:17:13 +0300

    added tests,edited bug in get method

--- src/main/java/org/korol/hashmap/OpenAddressingHashMap.java
+++ src/main/java/org/korol/hashmap/OpenAddressingHashMap.java
@@ -6,4 +6,137 @@
 package org.korol.hashmap;
 
 public class OpenAddressingHashMap {
+
+    private Entry[] table;
+    private int capacity;
+    private int size;
+    private double loadFactor;
+    private double amplificationCoefficient;
+
+    {
+        capacity=16;
+        loadFactor=0.75;
+        amplificationCoefficient=1.5;
+        size=0;
+    }
+
+    private int hash (int key){
+        return key % capacity;
+    }
+
+    public OpenAddressingHashMap(){
+        table=new Entry[capacity];
+    }
+    public OpenAddressingHashMap(int capacity){
+        this.capacity=capacity;
+        table=new Entry[capacity];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean put(int key,long value){
+        if (needsToAmplify())
+            amplifyTableWithNewSize();
+
+        int hash=hash(key);
+        if (table[hash]!=null){
+
+            if (table[hash].isDeleted()) {
+                table[hash] = new Entry(key, value);
+                size++;
+                return true;
+            }
+            for (int i = hash + 1; i != hash; i = (i + 1) % table.length) {
+                if (table[i]!=null) {
+                    if (table[i].isDeleted()) {
+                        table[i] = new Entry(key, value);
+                        size++;
+                        return true;
+                    }
+                }
+                else{
+                    table[i]=new Entry(key, value);
+                    size++;
+                    return true;
+                }
+            }
+        }
+        table[hash] = new Entry(key, value);
+        size++;
+        return true;
+    }
+
+    public long get(int key) {
+        int hash = hash(key);
+
+        if (table[hash] != null){
+            if (table[hash].getKey() == key && !table[hash].isDeleted()) {
+                return table[hash].getValue();
+            }
+        }
+            for (int i = hash + 1; i != hash; i = (i + 1) % table.length) {
+               if (table[i]!=null) {
+                   if (table[i].getKey() == key && !table[i].isDeleted()) {
+                       return table[i].getValue();
+                   }
+               }
+            }
+            return 0;
+
+
+
+    }
+    @Override
+    public String toString() {
+
+        StringBuilder stringBuilder=new StringBuilder();
+        for (int i = 0; i <table.length ; i++) {
+            if (table[i]!=null) {
+                stringBuilder.append(table[i].getKey()
+                        + ":" + table[i].getValue() + "\n");
+            }
+        }
+
+
+
+
+        return String.valueOf(stringBuilder);
+    }
+    public boolean remove(int key) {
+        int hash=hash(key);
+        try{
+            if (table[hash].getKey() == key) {
+                table[hash].deleteEntry();
+                return true;
+            }
+            for (int i = hash + 1; i != hash; i = (i + 1) % table.length) {
+                if (table[i].getValue() == key && !table[i].isDeleted()) {
+                    table[i].deleteEntry();
+                    return true;
+                }
+            }
+            return false;
+        } catch (NullPointerException e){
+            return false;
+        }
+    }
+
+    private boolean needsToAmplify(){
+        return size > capacity * loadFactor?true:false;
+    }
+
+    private void amplifyTableWithNewSize(){
+        size=0;
+        capacity = (int) (capacity*amplificationCoefficient);
+        Entry[] oldTable=table;
+        table=new Entry[capacity];
+        Entry temporary;
+        for (int i = 0; i <oldTable.length; i++) {
+            if (oldTable[i]!=null && !oldTable[i].isDeleted())
+            put(oldTable[i].getKey(),oldTable[i].getValue());
+        }
+
+    }
 }
---
+++ src/test/java/org/korol/hashmap/OpenAddressingHashMapTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2020.
+ * Anton Korol
+ */
+
+package org.korol.hashmap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.*;
+
+public class OpenAddressingHashMapTest  {
+
+    private final int INITIAL_SIZE=500;
+    private OpenAddressingHashMap hashMap;
+
+    @Before
+    public void setUp() {
+
+        hashMap=new OpenAddressingHashMap();
+    }
+
+
+
+    @Test
+    public void putTest(){
+
+    }
+
+    @Test
+    public void removeTest(){
+        int key=55;
+        long firstValue=1333;
+        long secondValue=2444;
+        hashMap.put(key,firstValue);
+        hashMap.put(key,secondValue);
+        hashMap.remove(key);
+        assertEquals(secondValue,hashMap.get(key));
+    }
+
+    @Test
+    public void getTest(){
+        int key=172;
+        long value=228;
+        hashMap.put(key,value);
+        assertEquals(value,hashMap.get(key));
+    }
+
+    @Test
+    public void sizeTest(){
+        int expectedSize=99;
+        for (int i = 0; i <expectedSize ; i++) {
+            hashMap.put(i*10,i+50);
+        }
+        assertEquals(expectedSize,hashMap.size());
+    }
+
+
+    @Test
+    public void amplificationOfCapacityTest() throws NoSuchFieldException, IllegalAccessException {
+        Field capacityField=OpenAddressingHashMap
+                .class.getDeclaredField("capacity");
+        capacityField.setAccessible(true);
+        int oldCapacity=capacityField.getInt(hashMap);
+
+        Field loadFactorField=OpenAddressingHashMap
+                .class.getDeclaredField("loadFactor");
+        loadFactorField.setAccessible(true);
+        double loadFactor=loadFactorField.getDouble(hashMap);
+
+        Field amplificationCoefficientField=OpenAddressingHashMap
+                .class.getDeclaredField("amplificationCoefficient");
+        amplificationCoefficientField.setAccessible(true);
+        double amplificationCoeffiecient=amplificationCoefficientField
+                .getDouble(hashMap);
+
+        for (int index=0;index<oldCapacity*loadFactor;index++){
+            hashMap.put((index+20)*5,index*20+100);
+        }
+        int newCapacity=capacityField.getInt(hashMap);
+
+        capacityField.setAccessible(false);
+        loadFactorField.setAccessible(false);
+        amplificationCoefficientField.setAccessible(false);
+
+        assertEquals(oldCapacity,newCapacity);
+    }
+
+}
